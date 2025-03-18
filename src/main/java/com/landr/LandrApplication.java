ac5bcca1fc45aa6f16a9d6fc39b26751bc488880
nick <andantej281@gmail.com>
2025-03-18 13:04:32 +0900

    init

---
+++ src/main/java/com/landr/LandrApplication.java
@@ -0,0 +1,13 @@
+package com.landr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/landr/LandrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
