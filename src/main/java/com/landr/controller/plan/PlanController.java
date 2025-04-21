afb4c11d3cbb0a72a9ca8802d8b059eae149ced1
nick <andantej281@gmail.com>
2025-04-21 17:51:24 +0900

    feat: 상세 계획 조회 개발 중

--- src/main/java/com/landr/controller/plan/PlanController.java
+++ src/main/java/com/landr/controller/plan/PlanController.java
@@ -13,6 +13,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping("/v1/plans")
